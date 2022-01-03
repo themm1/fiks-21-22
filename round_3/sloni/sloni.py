5b602e43b1d17c27bb910869b25d89a325c27348
themm1 <madzin.m@gmail.com>
2022-01-03 13:19:10 +0100

    sloni edited

--- round_3/sloni/sloni.py
+++ round_3/sloni/sloni.py
@@ -15,7 +15,6 @@ def get_fields_count(x_size, y_size, x_coord, y_coord, layers_count):
         if pyramid_height > 0:
             fields -= int(Decimal(pyramid_height) * (Decimal(pyramid_height) + 1) / 2)
 
-    for i, max_dir in enumerate(max_directions):
         second_index = i + 1
         if second_index == len(max_directions):
             second_index = 0
@@ -23,7 +22,8 @@ def get_fields_count(x_size, y_size, x_coord, y_coord, layers_count):
         n = layers_count - height + 2
         if n > 0:
             fields += seq_formula(n)
-    return int(fields)
+
+    return fields
 
 def seq_formula(n):
     m = int(n / 2)
@@ -31,7 +31,6 @@ def seq_formula(n):
         return m * (m - 1) + m
     else:
         return m * (m + 1)
-
 
 def main(filename):
     with open(filename, "r") as f:
@@ -42,11 +41,11 @@ def main(filename):
     for task in content[1:tasks_count+1]:
         args = [int(arg) for arg in task.split(" ")]
         result = get_fields_count(*args)
+        print(result)
         output.append(str(result))
     
     with open("./round_3/sloni/output.txt", "w", newline="") as f:
         f.writelines("\n".join(output))
 
-# fields = get_fields_count(52, 90, 20, 49, 920043000)
-# print(fields)
-main("./round_3/sloni/input.txt")
+if __name__ == "__main__":
+    main("./round_3/sloni/input.txt")
